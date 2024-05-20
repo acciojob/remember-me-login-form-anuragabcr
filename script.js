0a4c73db578830781c27b9028d4902dd4d23270f
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-20 13:37:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+let submit = document.getElementById("submit")
+let existing = document.getElementById("existing")
+let uname = localStorage.getItem("username")
+if (uname) {
+	existing.innerHTML = "Login as existing user."
+}
+
+existing.addEventListener("click", () => {
+	alert("Logged in as .")
+})
+
+submit.addEventListener("click", () => {
+	let username = document.getElementById("username").value
+	let password = document.getElementById("password").value
+	let checkbox = document.getElementById("checkbox")
+	if (checkbox.checked) {
+		localStorage.setItem('username', username)
+		localStorage.setItem('password', password)
+	} else {
+		localStorage.removeItem('username')
+		localStorage.removeItem('password')	
+	}
+	alert("Logged in as .")
+})
